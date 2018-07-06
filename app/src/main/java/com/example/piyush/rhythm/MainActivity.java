1eaa81d2043da5b5d0e9346aa3ff1cbaa895f699
piyush <piyush.sinha24@gmail.com>
2018-07-06 18:53:19 +0530

    Working on search interafce

--- app/src/main/java/com/example/piyush/rhythm/MainActivity.java
+++ app/src/main/java/com/example/piyush/rhythm/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.piyush.rhythm;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.media.MediaPlayer;
@@ -9,12 +11,17 @@ import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -26,13 +33,29 @@ import java.util.ArrayList;
 import java.util.jar.Manifest;
 
 public class MainActivity extends AppCompatActivity {
-
     private ArrayList<Songinfo> songs = new ArrayList<Songinfo>();
     RecyclerView recyclerView;
     SeekBar seekBar;
     SongAdapter songAdapter;
     MediaPlayer mp;
     Handler handler=new Handler();
+    private String sortorder;
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+
+        SearchManager searchManager = (SearchManager) MainActivity.this.getSystemService(Context.SEARCH_SERVICE);
+
+        SearchView searchView = (SearchView)menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
         DividerItemDecoration dividerItemDecoration=new DividerItemDecoration(recyclerView.getContext(),linearLayoutManager.getOrientation());
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.addItemDecoration(dividerItemDecoration);
+        sortorder = MediaStore.MediaColumns.DISPLAY_NAME+"";
         songAdapter.setOnItemClickListener(new SongAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(final Button b, View v, final Songinfo obj, int position) {
@@ -96,6 +120,9 @@ public class MainActivity extends AppCompatActivity {
         t.start();
 
     }
+
+
+
     public class runThread extends Thread {
         @Override
         public void run() {
@@ -174,12 +201,16 @@ public class MainActivity extends AppCompatActivity {
     private void loadsongs(){
         Uri uri= MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
         String selection = MediaStore.Audio.Media.IS_MUSIC+"!= 0";
-        Cursor cursor = getContentResolver().query(uri,null,selection,null,null);
+        Cursor cursor = getContentResolver().query(uri,null,selection,null,sortorder);
         if(cursor!=null)
         {
             if(cursor.moveToFirst()){
                 do {
                     String name = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME));
+                    name = name.replaceFirst("^\\s*[0-9]+\\s*", "");
+                    name = name.replaceFirst("^\\\\d+\\\\.?\\\\s*-(?:.*?-)?\\\\s*","");
+                    name = name.replaceFirst("\'","");
+                    name = name.replaceFirst("\"","");
                     String artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
                     String url = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
                     Songinfo s= new Songinfo(name,artist,url);
@@ -187,7 +218,6 @@ public class MainActivity extends AppCompatActivity {
 
                 }while (cursor.moveToNext());
                 Log.d("SongsList","run: "+songs);
-
 
 
             }
